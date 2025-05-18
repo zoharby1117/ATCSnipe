93f767fa3e0b9ef18aca8e3a699edb55a5c5a29d
zoharby1117 <zoharb23@gmail.com>
2025-05-17 21:43:47 -0400

    I'm trying to make 4 direction movement but it isnt working sadly

---
+++ ATCSnipe/Assets/Scripts/InputManager.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputManager : MonoBehaviour
+{
+    private PlayerInput playerInput; //player input is the c# class version of the inputs we defined in the inputs folder
+    private PlayerInput.OnFootActions onFoot; //onFoot action map
+
+    private PlayerMotor motor; //will have the PlayerMotor component of the object
+
+    void Awake() //awake is while the game is initialized, i.e. before start
+    {
+        playerInput = new PlayerInput();
+        onFoot = playerInput.OnFoot; //this is what we use for our on foot actions.
+        motor = GetComponent<PlayerMotor>(); //the playermotor component we dragged into the inspector of the Player object
+    }
+
+    void FixedUpdate() //not sure what the difference between this and update is
+    {
+        //tell the playermotor to move using the value from our movement action
+        //playermotor uses its ProcessMove method to move
+        motor.ProcessMove(onFoot.Movement.ReadValue<Vector2>());
+        //Movement is an action in the actionmap that outputs a value
+
+    }
+
+    //the following code enables or disables the onFoot action map, useful in situations when you shouldnt be moving
+    //such as when looking at photos
+    private void OnFootEnable()
+    {
+        onFoot.Enable();
+    }
+
+    private void OnFootDisable()
+    {
+        onFoot.Disable();
+    }
+}
