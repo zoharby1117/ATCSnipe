e228038ac66e0d24f32afd2e018366855385a91b
zoharby1117 <zoharb23@gmail.com>
2025-05-29 16:58:59 -0400

    basic faceplayer
    Not complete

--- ATCSnipe/Assets/Scripts/FacePlayer.cs
+++ ATCSnipe/Assets/Scripts/FacePlayer.cs
@@ -3,14 +3,15 @@ using UnityEngine;
 public class FacePlayer : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public Transform player;
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.LookAt(player);
     }
 }
