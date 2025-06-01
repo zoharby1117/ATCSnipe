c363dbbe56bb5fb28fa893b6ecffab77e915d4ff
darkcat773 <i7nanami3@gmail.com>
2025-06-01 11:37:28 -0400

    Revert "Nanami2"

--- ATCSnipe/Assets/Scripts/ViewPhotos.cs
+++ ATCSnipe/Assets/Scripts/ViewPhotos.cs
@@ -14,7 +14,6 @@ public class ViewPhotos : MonoBehaviour
 
     public static bool viewing;//uses bool not boolean apparently
 
-    public bool activated = true;
     private float temp;
 
     private ObjInfo[] tempPositions;
@@ -38,8 +37,6 @@ public class ViewPhotos : MonoBehaviour
             {
                 tempPositions = TakePhoto.generateArray();
                 Time.timeScale = 0;//freeze physics
-                
-                
                 viewPhoto();
             }
             else
@@ -76,18 +73,12 @@ public class ViewPhotos : MonoBehaviour
                 //end of copied code
                 //player.GetComponent<CharacterController>().minMoveDistance = temp;
                 Time.timeScale = 1;
-                TakenTextChanger.instance.enableTaking();
             }
 
         }
         if (viewing && album.Count > 1)
         {
 
-            TakenTextChanger.instance.disableTaking();
-            if (activated)
-            {
-                TakenTextChanger.instance.CurrentPhotoNum(0);
-            }
             //player.GetComponent<CharacterController>().minMoveDistance = 999;
 
             if (Input.GetKeyDown(KeyCode.RightArrow))
@@ -96,15 +87,11 @@ public class ViewPhotos : MonoBehaviour
                 {
                     i++;
                     viewPhoto();
-                    TakenTextChanger.instance.CurrentPhotoNum(i);
-                    activated = false;
                 }
                 else
                 {
                     i = 0;
                     viewPhoto();
-                    TakenTextChanger.instance.CurrentPhotoNum(i);
-                    activated = false;
                 }
 
 
@@ -115,15 +102,11 @@ public class ViewPhotos : MonoBehaviour
                 {
                     i--;
                     viewPhoto();
-                    TakenTextChanger.instance.CurrentPhotoNum(i);
-                    activated = false;
                 }
                 else
                 {
                     i = album.Count - 1;
                     viewPhoto();
-                    TakenTextChanger.instance.CurrentPhotoNum(i);
-                    activated = false;
                 }
 
             }
